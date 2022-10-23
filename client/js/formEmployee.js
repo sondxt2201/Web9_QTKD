9eeb75598fef447516373393bd6ac6c54c49bb5a
sondxt2201 <sondxt2201@gmail.com>
2022-10-24 04:32:52 +0700

    add js

--- client/js/formEmployee.js
+++ client/js/formEmployee.js
@@ -7,6 +7,8 @@
 window.onload = () => {
     initFormEvents();
 }
+const $ = document.getElementById.bind(document);
+const $$ = document.getElementsByClassName.bind(document);
 
 // Khởi tạo các sự kiện
 function initFormEvents() {
@@ -17,11 +19,11 @@ function initFormEvents() {
 }
 
 // Khai báo biến
-const $ = document.getElementById.bind(document)
 const btnAdd = $('btnAdd');
 const form = $('formEmployee');
-const btnEsc = $('btnEsc');
 const btnAddtodatabase = $('btnAddtodatabase');
+const btnEsc = $$("btnEsc");
+// console.log(btnEsc);
 
 /**
  * Mở formEmployee
@@ -29,14 +31,14 @@ const btnAddtodatabase = $('btnAddtodatabase');
  */
 function openForm() {
     try {
-    btnAdd.addEventListener('click', () => {
-        if (form.style.display === 'none') {
-            //  Mở form
-            form.style.display = 'flex';
-        } else {
-            form.style.display = 'none';
-        }
-    });
+        btnAdd.addEventListener('click', () => {
+            if (form.style.display === 'none') {
+                //  Mở form
+                form.style.display = 'flex';
+            } else {
+                form.style.display = 'none';
+            }
+        });
     } catch (error) {
         console.log(error)
     }
@@ -50,9 +52,12 @@ function openForm() {
  */
 function closeForm() {
     try {
-        btnEsc.addEventListener('click', () => {
-            form.style.display = 'none';
-        });
+        for (let btns of btnEsc) {
+            btns.addEventListener('click', () => {
+                form.style.display = 'none';
+            });
+        }
+
     } catch (error) {
         console.log(error)
     }
@@ -71,7 +76,7 @@ btnAddtodatabase.addEventListener('click', () => {
     var employeeCode = "";
 
     // gọi api cất dữ liệu
-    fetch("https://cukcuk.manhnv.net/api/v1/Customers", {
+    fetch("https://amis.manhnv.net/api/v1/Employees", {
         method: "POST",
         body: JSON.stringify(employee),
     })
@@ -90,31 +95,136 @@ btnAddtodatabase.addEventListener('click', () => {
  * Date: 21/10/2022
  */
 function validateData() {
+
     // Các thông tin bắt buộc
-    var employeeCode = $("txtEmployeeCode").value;
-    var fullName = $("txtFullName").value;
-    var dlgWrapper = $("dialog-wrapper");
-    var dlgEmployeeCode = $('dialog-warning-employeeCode')
-    var btnEsc1 = $('btnEsc1')
+    let employeeCode = $("txtEmployeeCode").value;
+    let fullName = $("txtFullName").value;
+    let dob = $("txtDateOfBirth").value;
+    let idNumber = $("txtIDNumber").value;
+    let department = $("txtDepartment").value;
+    let phoneNumber = $("txtPhoneNumber").value;
+    let email = $("txtEmail").value;
 
-    if (!employeeCode) {
-        // Đổi màu border
-        $('txtEmployeeCode').style.border = "2px solid #000";
 
-        // Hiển thị thông báo
-        dlgWrapper.style.display = "flex";
-        dlgEmployeeCode.style.display = "flex";
-        btnEsc1.addEventListener('click', () => {
+    let dlgbtnEsc = $$('dlg-btnEsc');
+    let dlgWrapper = $("dialog-wrapper");
+    let dlgEmployeeCode = $('dialog-warning-employeeCode');
+    let dlgFullName = $('dialog-warning-fullName');
+    let dlgRequired = $('dialog-warning-required');
+    let dlgDOB = $('dialog-warning-dob');
+    let dlgIDNumber = $('dialog-warning-idNumber');
+    let dlgDepartment = $('dialog-warning-department');
+    let dlgPhoneNumber = $('dialog-warning-phone');
+    let dlgEmail = $('dialog-warning-email');
+
+
+    // Tắt dialog khi ấn nút "Đóng"
+    for (let btns of dlgbtnEsc) {
+        btns.addEventListener('click', () => {
             dlgEmployeeCode.style.display = 'none';
+            dlgIDNumber.style.display = 'none';
+            dlgFullName.style.display = 'none';
+            dlgRequired.style.display = 'none';
+            dlgDOB.style.display = 'none';
             dlgWrapper.style.display = 'none';
         });
     }
+
+    // Kiểm tra employeeCode 
+    if (!employeeCode && fullName) {
+        // Đổi màu border
+        $('txtEmployeeCode').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "flex";
+        dlgEmployeeCode.style.display = "flex";
+
+    } else {
+        $('txtEmployeeCode').style.border = "1px solid #CEEAD9";
+    }
+
+    // Kiểm tra FullName
+    if (!fullName && employeeCode) {
+        // Đổi màu border
+        $('txtFullName').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "flex";
+        dlgFullName.style.display = "flex";
+    } else {
+        $('txtFullName').style.border = "1px solid #CEEAD9";
+    }
+
+    // Kiểm tra fullName và employeeCode
+    if (!employeeCode && !fullName) {
+        // Đổi màu border
+        $('txtFullName').style.border = "1px solid #000";
+        $('txtEmployeeCode').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "flex";
+        dlgRequired.style.display = "flex";
+    } else {
+        $('txtFullName').style.border = "1px solid #CEEAD9";
+        $('txtEmployeeCode').style.border = "1px solid #CEEAD9";
+    }
+
+    // Kiểm tra ngày sinh
+    if (dob) {
+        dob = new Date(dob);
+    }
+    if (dob > new Date()) {
+        // alert('Ngày sinh không được lớn hơn ngày hiện tại');
+        // Đổi màu border
+        $('txtDateOfBirth').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "block";
+        dlgDOB.style.display = "flex";
+    } else {
+        $('txtDateOfBirth').style.border = "1px solid #CEEAD9";
+    }
+
+    // Kiểm tra số CMND
+    var idCheck = /([0-9]{9}) | ([0-9]{12})$/;
+    if (!idNumber) {
+        // Đổi màu border
+        $('txtIDNumber').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "block";
+        dlgIDNumber.style.display = "flex";
+    } else if (idNumber != null && !idNumber.match(idCheck)) {
+        alert("sai CMND");
+        // $('txtIDNumber').style.border = "1px solid #CEEAD9";
+    }
+
+    // Kiểm tra đơn vị
+    if (!department) {
+        // Đổi màu border
+        $('txtDepartment').style.border = "1px solid #000";
+        // Hiển thị thông báo
+        dlgWrapper.style.display = "block";
+        dlgDepartment.style.display = "flex";
+    } else {
+        $('txtDepartment').style.border = "1px solid #CEEAD9";
+    }
+    
+    // Các thông tin cần đúng định dạng
+    
+    let phoneCheck = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/;
+    if (phoneNumber && phoneNumber.match(phoneCheck) == null) {
+        dlgWrapper.style.display = "block";
+        dlgPhoneNumber.style.display = "flex";
+    }
     else {
-        $('txtEmployeeCode').style.border = "1px solid #ccc";
+        $('txtPhoneNumber').style.border = "1px solid #CEEAD9";
 
     }
-    // Các thông tin cần đúng định dạng
-
+    
+    let emailCheck = /^[a-z0-9!#$%&'*+/=?^_{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_{|}~-]+)_@(?:[a-z0-9](?:[a-z0-9-]_[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+    if (email & email.match(emailCheck) == null) {
+        dlgWrapper.style.display = "block";
+        dlgEmail.style.display = "flex";
+    }
+    else {
+        $('txtEmail').style.border = "1px solid #CEEAD9";
+    }
     // Định dạng ngày tháng
 }
 
--- client/js/table.js
+++ client/js/table.js
@@ -4,35 +4,39 @@
  */
 
 // Gọi hàm loadData()
-window.onload = () => {
-    loadData();
-}
+loadData();
+const $ = document.getElementById.bind(document);
+const $$ = document.getElementsByClassName.bind(document);
 
 // Khai báo biến
-const $ = document.getElementById.bind(document);
 const btnReload = $('btnReload');
 const tableBody = $("employeeList").children[1]
 const template = ({
-    CustomerCode,
-    FullName,
+    EmployeeCode,
+    EmployeeName,
     Gender,
     DateOfBirth,
+    IdentityNumber,
+    PositionName,
+    DepartmentName,
+    BankAccountNumber,
+    BankName,
+    BankBranchName,
 }) => `
         <tr>
             <td data-type="EmployeeCode" class="text-align--center no_padding column-tiny-width">
                 <input type="checkbox">
             </td>
-            <td class="text-align--left column-medium-width">${CustomerCode}</td>
-            <td class="text-align--left column-large-width">${FullName}</td>
-            <td class="text-align--left column-small-width">${Gender === 2 ? "Nam" : "Nữ"}</td>
+            <td class="text-align--left column-medium-width">${EmployeeCode}</td>
+            <td class="text-align--left column-large-width">${EmployeeName}</td>
+            <td class="text-align--left column-small-width">${Gender === 0 ? "Nam" : "Nữ"}</td>
             <td class="text-align--center no_padding column-medium-width">${DateOfBirth}</td>
-            <td class="text-align--left column-big-width">123456789876</td>
-            <td class="text-align--left column-large-width">Nhân viên</td>
-            <td class="text-align--left column-large-width">Nhóm kiểm hàng</td>
-            <td class="text-align--left column-medium-width">Số tài khoản</td>
-            <td class="text-align--left column-large-width">Tên ngân hàng</td>
-            <td class="text-align--left no-right-border olumn-large-width">Chi nhánh TKngân hàng
-            </td>
+            <td class="text-align--left column-big-width">${IdentityNumber}</td>
+            <td class="text-align--left column-large-width">${PositionName}</td>
+            <td class="text-align--left column-large-width">${DepartmentName}</td>
+            <td class="text-align--left column-medium-width">${BankAccountNumber}</td>
+            <td class="text-align--left column-large-width">${BankName}</td>
+            <td class="text-align--left no-right-border column-large-width">${BankBranchName}</td>
             <td class="text-align--center no_padding table-anchor-right column-small-width">
                 <div class="table-combobox combobox">
                     <button href="" class="table-combobox-title button">Sửa</button>
@@ -57,17 +61,17 @@ const template = ({
  */
 async function loadData() {
     try {
-        const data = await fetch("https://cukcuk.manhnv.net/api/v1/Customers", {
+        const data = await fetch("https://amis.manhnv.net/api/v1/Employees", {
             method: "GET"
         })
             .then(res => res)
-            .then(res => res.json());
-        tableBody.innerHTML = data.map(item => template(item)).join("");
+            .then(res => res.json())
+            tableBody.innerHTML = data.map(item => template(item)).join("")
+            // console.log(data)
     } catch (error) {
         console.log(error)
     }
 }
-loadData();
 
 
 
